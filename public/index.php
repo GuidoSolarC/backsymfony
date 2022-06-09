f68a61e510a2ce27c0b5382f612f06ab5d7c4506
Guido Solar <guido.solar.c@gmail.com>
2022-06-09 16:44:13 -0400

    Refactor de API

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 header("Origin: http://localhost:8000");
 header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Methods: *');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
--- src/Controller/PruebaController.php
+++ src/Controller/PruebaController.php
@@ -10,20 +10,20 @@ use Symfony\Component\HttpFoundation\Request;
 class PruebaController extends AbstractController
 {
     /**
-     * @Route("/prueba/numerosPrimos", name="numerosPrimos")
+     * @Route("/prueba/numerosPrimos", name="numerosPrimos", methods={"GET","HEAD"})
      */
-    public function numerosPrimos(Request $request)  
-    {
-        if($request->getMethod() == 'POST'){          
-            // Contenemos el valor       
-            $n = $request->request->get('valor');
+    public function numerosPrimos(Request $request)
+    {       
+        // Contenemos el valor       
+        $valor = is_numeric($request->get('valor')) ? (int)$request->get('valor') : 0;
+        if($valor !== 0){
             // Declaro un array vacio para ir conteniendo los numeros primos 
             $arrayPrimos = [];
 
             // https://learnetutorials.com/php/programs/print-n-prime-numbers
             $count = 0;
             $num = 2;
-            while ($count < $n) {
+            while ($count < $valor) {
                 $dcount = 0;
                 for ($i = 2; $i <= $num; $i++) {
                     if (($num % $i) == 0) {
@@ -37,20 +37,18 @@ class PruebaController extends AbstractController
                 $num++;
             }
 
-
-            return new JsonResponse([
-                'msg' => 'Respuesta', 
-                'valorRecibido' => $n, 
+            return new JsonResponse(array(
+                'msg' => 'Respuesta correcta', 
+                'valorRecibido' => $valor, 
                 'numerosPrimos' => $arrayPrimos
-            ], Response::HTTP_OK);
-
+            ), Response::HTTP_OK);
         } else {
-            return new JsonResponse([
-                'msg' => 'Método incorrecto',
-                'valorRecibido' => 0,
-                'numerosPrimos' => 0
-            ], Response::HTTP_BAD_REQUEST);
-        }    
+            return new JsonResponse(array(
+                'msg' => 'El valorg no es un número', 
+                'valorRecibido' => $request->get('valor'), 
+                'numerosPrimos' => '----'
+            ), Response::HTTP_OK);
+        }
     }
 
 
